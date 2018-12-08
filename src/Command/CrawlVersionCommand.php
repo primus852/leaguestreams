fd50995a0178da394d4eed97a33926f6628464d4
PrimuS <findnibbler@gmail.com>
2018-12-08 09:24:03 +0100

    Crawl Versions Command more Debug options

--- src/Command/CrawlVersionCommand.php
+++ src/Command/CrawlVersionCommand.php
@@ -2,10 +2,15 @@
 
 namespace App\Command;
 
+use App\Entity\Champion;
+use App\Entity\Versions;
 use App\Utils\Locker\Locker;
 use App\Utils\Locker\LockerException;
 use App\Utils\LS\Crawl;
 use App\Utils\LS\LSException;
+use App\Utils\RiotApi\RiotApi;
+use App\Utils\RiotApi\RiotApiException;
+use App\Utils\RiotApi\Settings;
 use Doctrine\Common\Persistence\ObjectManager;
 use primus852\SimpleStopwatch\Stopwatch;
 use primus852\SimpleStopwatch\StopwatchException;
@@ -87,11 +92,92 @@ class CrawlVersionCommand extends Command
         try{
             $lsCrawl->versions();
             $debug ? $io->text('Updated Versions: <fg=green>success</>') : null;
-            $lsCrawl->update_champions();
-            $debug ? $io->text('Updated Champions: <fg=green>success</>') : null;
         }catch (LSException $e){
             throw new LSException('Could not gather Versions: '.$e->getMessage());
         }
+
+        /**
+         * Gather all champions from ddragon
+         */
+        $champions = $lsCrawl->update_champions();
+        $debug ? $io->text('Gathered all Champions: <fg=green>success</>') : null;
+
+        /**
+         * Loop through champions
+         * @var $champion Champion
+         */
+        foreach($champions['data'] as $champion_data){
+
+            $id = (int)$champion_data['key'];
+
+            /**
+             * Check if Champion exists
+             */
+            $champion = $this->em->getRepository(Champion::class)->find($id);
+
+            $status = '<fg=yellow>updated</>';
+            if($champion === null){
+                $champion = new Champion();
+                $champion->setId($id);
+                $status = '<fg=green>created</>';
+            }
+
+            $name = $champion_data['id'];
+
+            /**
+             * Use NA1 for Static
+             */
+            $api = new RiotApi(new Settings());
+
+            /**
+             * Get the current Version
+             */
+            $version = $this->em->getRepository(Versions::class)->find(1);
+
+            try {
+                $champion_api = $api->getChampion($version->getChampion(), $name);
+            } catch (RiotApiException $e) {
+                throw new LSException('Gather Versions Exception: ' . $e->getMessage());
+            }
+
+            $champion->setModified(new \DateTime());
+            $champion->setName($name);
+            $champion->setTitle($champion_api['data'][$name]['title']);
+            $champion->setImage($champion_api['data'][$name]['image']['full']);
+            $champion->setChampKey($champion_api['data'][$name]['id']);
+            $champion->setBlurb($champion_api['data'][$name]['blurb']);
+            $champion->setLore($champion_api['data'][$name]['lore']);
+            $champion->setSpellPassiveImage($champion_api['data'][$name]['passive']['image']['full']);
+            $champion->setSpellPassiveName($champion_api['data'][$name]['passive']['name']);
+            $champion->setSpellPassiveDescription($champion_api['data'][$name]['passive']['description']);
+            $champion->setSpellQImage($champion_api['data'][$name]['spells'][0]['image']['full']);
+            $champion->setSpellQName($champion_api['data'][$name]['spells'][0]['name']);
+            $champion->setSpellQDescription($champion_api['data'][$name]['spells'][0]['description']);
+            $champion->setSpellEImage($champion_api['data'][$name]['spells'][1]['image']['full']);
+            $champion->setSpellEName($champion_api['data'][$name]['spells'][1]['name']);
+            $champion->setSpellEDescription($champion_api['data'][$name]['spells'][1]['description']);
+            $champion->setSpellWImage($champion_api['data'][$name]['spells'][2]['image']['full']);
+            $champion->setSpellWName($champion_api['data'][$name]['spells'][2]['name']);
+            $champion->setSpellWDescription($champion_api['data'][$name]['spells'][2]['description']);
+            $champion->setSpellRImage($champion_api['data'][$name]['spells'][3]['image']['full']);
+            $champion->setSpellRName($champion_api['data'][$name]['spells'][3]['name']);
+            $champion->setSpellRDescription($champion_api['data'][$name]['spells'][3]['description']);
+            $this->em->persist($champion);
+
+            $debug ? $io->text('-->Champion: <fg=green>'.$champion->getName().'</>: '.$status) : null;
+
+        }
+
+        $debug ? $io->text('Flushing DB:') : null;
+
+        try{
+            $this->em->flush();
+            $debug ? $io->text('--><fg=green>success</>') : null;
+        }catch (\Exception $e){
+            throw new LSException('MySQL Error: '.$e->getMessage());
+        }
+
+
 
         /**
          * Remove the Lockfile
